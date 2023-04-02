63a50a51824169ed40d3c7e10325d312b92855fd
rockice177 <81545651+rockice177@users.noreply.github.com>
2023-04-01 21:51:36 -0400

    Update to add server request.
    Remember to replace http://your_server_url.com/data_receiver with the actual URL of your server that will handle the data.
    
    Now, this code will capture the input events from both the joystick and keyboard and send them to your server in real-time.

--- input_capture.py
+++ input_capture.py
@@ -1,4 +1,6 @@
 import pygame
+import json
+import requests
 
 # Initialize pygame
 pygame.init()
@@ -14,6 +16,18 @@ joysticks = [pygame.joystick.Joystick(x) for x in range(joystick_count)]
 for joystick in joysticks:
     joystick.init()
 
+# Server URL
+SERVER_URL = "http://your_server_url.com/data_receiver"
+
+# Function to send data to the server
+def send_data_to_server(data):
+    headers = {'Content-Type': 'application/json'}
+    try:
+        response = requests.post(SERVER_URL, data=json.dumps(data), headers=headers)
+        print(f"Data sent. Server response: {response.status_code}")
+    except Exception as e:
+        print(f"Error sending data to server: {e}")
+
 # Main event loop
 running = True
 while running:
@@ -24,10 +38,12 @@ while running:
         # Capture joystick events
         if event.type == pygame.JOYAXISMOTION or event.type == pygame.JOYBALLMOTION or event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP or event.type == pygame.JOYHATMOTION:
             print("Joystick event:", event)
+            send_data_to_server({"joystick_event": str(event)})
 
         # Capture keyboard events
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             print("Keyboard event:", event)
+            send_data_to_server({"keyboard_event": str(event)})
 
 # Clean up
 for joystick in joysticks:
