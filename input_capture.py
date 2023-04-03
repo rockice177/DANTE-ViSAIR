ef48a33fd9c026a964fe4152a063c83dce3df0b5
rockice177 <81545651+rockice177@users.noreply.github.com>
2023-04-02 22:09:36 -0400

    Update to input_capture.py - Start capturing
    Update to code

--- input_capture.py
+++ input_capture.py
@@ -1,53 +1,41 @@
 import pygame
-import json
-import requests
 
-# Initialize pygame
-pygame.init()
+def start_input_capture():
+    # Initialize pygame
+    pygame.init()
 
-# Set up the display
-screen = pygame.display.set_mode((640, 480))
+    # Set up the display
+    screen = pygame.display.set_mode((640, 480))
 
-# Set up joystick support
-pygame.joystick.init()
-joystick_count = pygame.joystick.get_count()
+    # Set up joystick support
+    pygame.joystick.init()
+    joystick_count = pygame.joystick.get_count()
 
-joysticks = [pygame.joystick.Joystick(x) for x in range(joystick_count)]
-for joystick in joysticks:
-    joystick.init()
+    joysticks = [pygame.joystick.Joystick(x) for x in range(joystick_count)]
+    for joystick in joysticks:
+        joystick.init()
 
-# Server URL
-SERVER_URL = "http://your_server_url.com/data_receiver"
+    # Main event loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-# Function to send data to the server
-def send_data_to_server(data):
-    headers = {'Content-Type': 'application/json'}
-    try:
-        response = requests.post(SERVER_URL, data=json.dumps(data), headers=headers)
-        print(f"Data sent. Server response: {response.status_code}")
-    except Exception as e:
-        print(f"Error sending data to server: {e}")
+            # Capture joystick events
+            if event.type in (pygame.JOYAXISMOTION, pygame.JOYBALLMOTION, pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP, pygame.JOYHATMOTION):
+                print("Joystick event:", event)
 
-# Main event loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+            # Capture keyboard events
+            if event.type in (pygame.KEYDOWN, pygame.KEYUP):
+                print("Keyboard event:", event)
 
-        # Capture joystick events
-        if event.type == pygame.JOYAXISMOTION or event.type == pygame.JOYBALLMOTION or event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP or event.type == pygame.JOYHATMOTION:
-            print("Joystick event:", event)
-            send_data_to_server({"joystick_event": str(event)})
+    # Clean up
+    for joystick in joysticks:
+        joystick.quit()
 
-        # Capture keyboard events
-        if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            print("Keyboard event:", event)
-            send_data_to_server({"keyboard_event": str(event)})
+    pygame.joystick.quit()
+    pygame.quit()
 
-# Clean up
-for joystick in joysticks:
-    joystick.quit()
-
-pygame.joystick.quit()
-pygame.quit()
+if __name__ == "__main__":
+    start_input_capture()
